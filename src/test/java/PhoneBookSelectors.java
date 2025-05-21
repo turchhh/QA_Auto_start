f85321eae8f9b543acbbcbf1949a1536e63e0fc8
turchhh <vturchannikova@gmail.com>
2025-05-21 17:21:37 +0300

    HW

--- src/test/java/PhoneBookSelectors.java
+++ src/test/java/PhoneBookSelectors.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -15,5 +17,24 @@ public class PhoneBookSelectors {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 //        HW
         FirstTest.pause(3);
+        WebElement btnAbout = driver.findElement(By.cssSelector("a[href='/about']"));
+        btnAbout.click();
+        FirstTest.pause(3);
+        WebElement btnLogin = driver.findElement(By.cssSelector("a[href='/login']"));
+        btnLogin.click();
+        FirstTest.pause(3);
+        WebElement inputEmail = driver.findElement(By.name("email"));
+        inputEmail.sendKeys("vturchannikova@gmail.com");
+        FirstTest.pause(2);
+        WebElement inputPassword = driver.findElement(By.cssSelector("*[placeholder='Password']"));
+        inputPassword.sendKeys("678512Lera!");
+        FirstTest.pause(2);
+        WebElement btnLogin1 = driver.findElement(By.name("login"));
+        btnLogin1.click();
+
+
+
+
+
     }
 }
